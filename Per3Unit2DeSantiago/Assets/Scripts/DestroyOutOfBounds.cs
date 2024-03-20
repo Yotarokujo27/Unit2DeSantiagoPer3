bf01c7f38834d6ceb25b52a1b433dc412b55c619
Alfredo De Santiago <727601@student.dusd.net>
2024-03-20 11:41:49 -0700

    almost done
    almost done with extras

--- Per3Unit2DeSantiago/Assets/Scripts/DestroyOutOfBounds.cs
+++ Per3Unit2DeSantiago/Assets/Scripts/DestroyOutOfBounds.cs
@@ -6,6 +6,7 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
     private float lowerBound = -10;
+    private float sideBound = 30;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,14 +16,24 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > 30)
+        if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         } else if (transform.position.z < lowerBound)
-           
         {
             Debug.Log("Game Over!");
             Destroy(gameObject);
         }
+        else if (transform.position.x > sideBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+        else if (transform.position.x < -sideBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+        
     }
 }
--- Per3Unit2DeSantiago/Assets/Scripts/DetectCollisions.cs
+++ Per3Unit2DeSantiago/Assets/Scripts/DetectCollisions.cs
@@ -16,9 +16,17 @@ public class DetectCollisions : MonoBehaviour
         
     }
 
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
-        Destroy(other.gameObject);
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Game Over");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            Destroy(other.gameObject);
+        }
     }
 }
